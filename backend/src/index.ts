25ac547068a5d2a334550e85fe8ab8480653a36a
Anuj Verma <anujvermagkp@gmail.com>
2025-04-21 18:14:59 +0530

    new file:   .gitignore 	new file:   backend/.env.example 	new file:   backend/package-lock.json 	new file:   backend/package.json 	new file:   backend/src/common/utils/get-env.ts 	new file:   backend/src/config/app.config.ts 	new file:   backend/src/database/models/database.ts 	new file:   backend/src/index.ts 	new file:   backend/tsconfig.json

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import "dotenv/config";
+import cors from "cors";
+import express,{Request,Response} from "express";
+import cookieParser from "cookie-parser";
+import { config } from "./config/app.config";
+
+const app = express();
+// const BASE_PATH = config.BASE_PATH;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    cors({
+    origin: config.APP_ORIGIN,
+    credentials: true,
+    
+})
+);
+
+app.use(cookieParser());
+
+app.get("/" ,(req : Request, res : Response) => {
+    res.status(200).json({
+        message: "Welcome to the API",
+    });
+});
+app.listen(config.APP_PORT, async () => {
+    console.log(`Server is running on port ${config.APP_PORT} in ${config.NODE_ENV}`);
+});
+
